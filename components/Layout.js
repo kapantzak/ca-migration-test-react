da5161ae1d48f26d537d6bf8b65884e3fb2c9add
John Kapantzakis <kapantzak@gmail.com>
2022-09-16 11:00:22 +0300

    Add test/one page

--- components/Layout.js
+++ components/Layout.js
@@ -14,11 +14,17 @@ const Layout = ({ children }) => {
         <Link href="/">
           <a>Home</a>
         </Link>
-        <Link href="/article">
-          <a>Article index</a>
+        <Link href="/test/one">
+          <a>One</a>
         </Link>
-        <Link href="/article/show">
-          <a>Article show</a>
+        <Link href="/test/two">
+          <a>Two</a>
+        </Link>
+        <Link href="/test/three">
+          <a>Three</a>
+        </Link>
+        <Link href="/test/four">
+          <a>Four</a>
         </Link>
       </nav>
       <main>{children}</main>
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ export default function Home() {
   return (
     <>
       <h1>New app</h1>
+      <p>Start the rails app in port 3000</p>
+      <p>There are 4 pages (One, Two, Three, Four) in the old (rails) app.</p>
+      <p>
+        This Nextjs app will redirect any not found path to the fallback url
+        (Rails app)
+      </p>
     </>
   );
 }
